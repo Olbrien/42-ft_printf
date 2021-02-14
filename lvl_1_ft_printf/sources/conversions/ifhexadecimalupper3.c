4a4bb70c9bee8f24567db673ed8bdf5f47a48e27
Olbrien <77759096+Olbrien@users.noreply.github.com>
2021-02-14 04:54:40 +0000

    Add files via upload

---
+++ lvl_1_ft_printf/sources/conversions/ifhexadecimalupper3.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ifhexadecimalupper3.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tisantos <tisantos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/24 22:23:07 by tisantos          #+#    #+#             */
+/*   Updated: 2021/02/10 08:22:39 by tisantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/ft_printf.h"
+
+static int	find_char(char *send)
+{
+	int	i;
+
+	i = 0;
+	while (send[i] != 'X' && send[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*hexaupper_write_zeros2(t_slist *slist, char *send,
+										char *string)
+{
+	if (string[0] == '-')
+		send = ft_strjoin(send, string + 1);
+	else
+		send = ft_strjoin(send, string);
+	if (ft_strchr(send, 'X') && ft_strlen(send) > ft_strlen(string))
+	{
+		send[find_char(send)] = '0';
+		send[1] = 'X';
+	}
+	send = hexadecimalupper_precision_with_zeros(slist, string, send);
+	return (send);
+}
+
+char	*return_if_one_hexaupperdigit(t_slist *slist, char *send,
+										long long int value)
+{
+	char	*temp;
+
+	temp = ft_itoa(value);
+	send = ft_strjoin(send, temp);
+	if (slist->precision >= 0)
+		send = reverse_hexadecimalupper(send, slist);
+	free(temp);
+	return (send);
+}
---
+++ lvl_1_ft_printf/sources/specifier.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   specifier.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tisantos <tisantos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/16 07:22:17 by tisantos          #+#    #+#             */
+/*   Updated: 2021/02/07 06:39:22 by tisantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+void	specifier(t_plist *plist, va_list *args, t_slist *slist)
+{
+	hasflags(plist, slist, args);
+	haswidth(plist, slist, args);
+	hasprecision(plist, slist, args);
+	haslength(plist, slist);
+	parse(plist, args, slist);
+}
